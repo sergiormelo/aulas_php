99e7358b196619139610c57ec8d539ed48d0aec1
Sergio Melo <srmsf93@protonmail.com>
2022-10-20 12:39:36 +0000

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   CSS/index.css 	modified:   controle1/operadores_logicos.php

--- controle1/operadores_logicos.php
+++ controle1/operadores_logicos.php
@@ -4,38 +4,49 @@ echo '<p>V ou F</p><hr>';
 var_dump(true);
 echo '<br>';
 var_dump(!true); //not
-echo "<p> Tabela Verdade 'AND' (E)</p><hr>";
+echo "<p class=divisao>> Tabela Verdade 'AND' (E)</p><hr>";
 echo '<br>' . var_dump(true && true);
 echo '<br>' . var_dump(true && false);
 echo '<br>' . var_dump(false && true);
 echo '<br>' . var_dump(false && false);
 // AND com outro operador:
+echo '<i>AND com outro operador:</i><br>';
 echo '<br>' . var_dump(true and true);
 echo '<br>' . var_dump(true and false);
 echo '<br>' . var_dump(false and true);
 echo '<br>' . var_dump(false and false);
-echo "<p> Tabela Verdade 'OR' (E)</p><hr>";
+echo "<p class=divisao>> Tabela Verdade 'OR' (E)</p><hr>";
 echo '<br>' . var_dump(true || true);
 echo '<br>' . var_dump(true || false);
 echo '<br>' . var_dump(false || true);
 echo '<br>' . var_dump(false || false);
 // OR com outro operador:
+echo '<i>OR com outro operador:</i><br>';
 echo '<br>' . var_dump(true or true);
 echo '<br>' . var_dump(true or false);
 echo '<br>' . var_dump(false or true);
 echo '<br>' . var_dump(false or false);
-echo "<p> Tabela Verdade 'XOR' (E)</p><hr>";
+echo "<p class=divisao> Tabela Verdade 'XOR' (E)</p><hr>";
 echo '<br>' . var_dump(true xor true);
 echo '<br>' . var_dump(true xor false);
 echo '<br>' . var_dump(false xor true);
 echo '<br>' . var_dump(false xor false);
+// XOR com outro operador:
+echo '<i>XOR com outro operador:</i><br>';
+echo '<br>' . var_dump(true xor true);
+echo '<br>' . var_dump(true xor false);
+echo '<br>' . var_dump(false xor true);
+echo '<br>' . var_dump(false xor false);
+echo "<p class='divisao'>Exemplo:</p><hr>";
+$idade=62;
+$sexo='F';
+if($idade>=60 && $sexo==='F'){
+    echo "Pode se aposentar --> $sexo";
+}
+else if($idade>=65 && $sexo==='M'){
+    echo "Pode se aposentar --> $sexo";
+}
+else {
+    echo "Vai ter que trabalhar mais um pouco...";
+}
 ?>
-<style>
-    p{
-        margin-bottom: -0.5rem;
-        font-weight: bold;
-    }
-    hr{
-        margin-bottom: 0rem;
-    }
-</style>
