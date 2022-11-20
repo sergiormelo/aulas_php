6035a03279c3f7b6381762ff26b17921c1559964
Sergio Melo <srmsf93@protonmail.com>
2022-11-20 21:11:32 +0000

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   classe_objetos/heranca.php

--- classe_objetos/heranca.php
+++ classe_objetos/heranca.php
@@ -1,5 +1,6 @@
 <div class="titulo">Herança</div>
 <?php
+//Classe
 class Pessoa{
     public $nome;
     public $idade;
@@ -9,9 +10,35 @@ class Pessoa{
         echo "Pesssoa Criada! <br>";
     }
     function __destruct(){
-        echo "Tchau!!";
+        echo "Pessoa diz: Tchau!!";
     }
     public function apresentar(){
-        echo "{$this->nome},{$this->idade} anos<br>";
+        echo "{$this->nome}, {$this->idade} anos<br>";
     }
 }
+//Subclasse
+class Usuario extends Pessoa{
+    public $login;
+    function __construct($nome, $idade, $login){
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->login = $login;
+        echo 'Usuário Criado!<br>';        
+    }
+    function __destruct(){
+        echo 'Usuário diz: Tchau!!<br>';
+        //Posso chamar também o destrutor da classe Pessoa!
+        parent::__destruct();
+    }
+    public function apresentar(){
+        // Maneira 1!
+        // echo "@{$this->login}: {$this->nome}, {$this->idade} anos.<br>";
+        // Maneira 2!
+        echo "@{$this->login}: ";
+        // Chamar a função apresentar da classe Pessoa.
+        parent::apresentar();
+    }
+}
+$usuario = new Usuario('Gustavo Mendonça', 21, 'gust_mend');
+$usuario->apresentar();
+unset($usuario);
